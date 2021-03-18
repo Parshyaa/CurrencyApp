e330b29840adb9c7f8821fae10b0fce02a965018
Parshyaa <49205781+Parshyaa@users.noreply.github.com>
2021-03-18 22:55:17 +0100

    Add files via upload

---
+++ CurrencyConverter/CurrencyConverter/Utilities/Interfaces/IUserSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.Utilities.Interfaces
+{
+    public interface IUserSettings
+    {
+        public string ApiKey { get;  }
+    }
+}
---
+++ CurrencyConverter/CurrencyConverter/Utilities/UserSettings.cs
@@ -0,0 +1,27 @@
+﻿using CurrencyConverter.Utilities.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.Utilities
+{
+    public class UserSettings : IUserSettings
+    {
+        private IConfigurationRoot Root;
+        private IConfigurationSection ApplicationSettings;
+        public UserSettings()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+
+            Root = configurationBuilder.Build();
+            ApplicationSettings = Root.GetSection("UserSettings");
+        }
+        public string ApiKey => ApplicationSettings["ApiKey"];
+    }
+}
